04907b53692184ed83853251425bb20fec1873ce
nastyayasko <nastya.stankevich@gmail.com>
2019-06-05 13:10:17 +0300

    fixed individual dialogs

--- index.js
+++ index.js
@@ -9,47 +9,70 @@ const server = app.listen(3020, () => {
 app.use(express.static('public'));
 
 let clients = [];
-let messages = [];
 let emails = [];
 let rooms = {};
 let blackList = {};
+
+const dialogs = [
+  {
+    type: 'global',
+    users: [],
+    messages: [],
+  }
+]
 
 const io = socket(server);
 
 io.on('connection', (socket) => {
   socket.emit('connect');
+
+  
+
   socket.on('email', (email) => {
     socket.email = email;
     if (!emails.includes(email)) {emails.push(email);};
     clients.forEach(client => {
       client.emit('people-online', emails); 
-    })
+    });
+    dialogs[0].users = emails;
+    const myDialogs = dialogs.filter(dialog => dialog.users.includes(socket.email));
+    socket.emit('dialogs', myDialogs);
   })
 
   socket.on('chat', (message) => {
-    messages.push(message);
-    if (rooms[socket.email]) {
+    console.log(message);
+    const {currentDialog} = message;
+
+    const current = dialogs.find(dialog => dialog.type === currentDialog);
+    
+    if (current) {
+      current.messages.push(message);
       clients.forEach(client => {
-        if (client.email === socket.email || client.email === rooms[socket.email]) {
+        if (current.users.includes(client.email)) {
+          if (blackList[socket.email] && blackList[socket.email].includes(client.email)){ return };
+          if (blackList[client.email] && blackList[client.email].includes(socket.email)){ return };
           client.emit('chat', message);
         }
       })
     } else {
+      
+      console.log(currentDialog);
+      console.log(socket.email);
+      const individual = dialogs.find(dialog => dialog.type === 'individual' && dialog.users.includes(currentDialog) && dialog.users.includes(socket.email));
+    
+      individual.messages.push(message);
       clients.forEach(client => {
-        if (blackList[socket.email] && blackList[socket.email].includes(client.email)){ return };
-        if (blackList[client.email] && blackList[client.email].includes(socket.email)){ return };
-        if (client.status) {
-          client.emit('chat', message); 
+        if (individual.users.includes(client.email)) {
+          if (blackList[socket.email] && blackList[socket.email].includes(client.email)){ return };
+          if (blackList[client.email] && blackList[client.email].includes(socket.email)){ return };
+          client.emit('chat', message);
         }
       })
     }
   })
  
   socket.on('connect-user', (user) => {
-    if (!socket.status) {
-      socket.emit('status', `You are in private chat with '${rooms[socket.email]}' now!`); 
-      return;
-    }
+    
     if (blackList[socket.email] && blackList[socket.email].includes(user)){
       socket.emit('status', `You blocked '${user}'.`); 
       return;
@@ -58,16 +81,39 @@ io.on('connection', (socket) => {
       socket.emit('status', `You can't chat with '${user}'. You are blocked.`); 
       return;
     }
-    const person = clients.find(client => client.status && client.email === user);
-    if (!person){ return socket.emit('status',`'${user}' is busy!`)};
-    person.status = false;
-    socket.status = false;
-    rooms[socket.email] = user;
-    rooms[user] = socket.email;
-    person.emit('status',`You are invited to private chat with '${socket.email}'!`);
-    socket.emit('status',`You can start chat with '${user}'.`);
-    person.emit('clear');
-    socket.emit('clear');
+    
+    const currentDialog = dialogs.find(dialog => dialog.type === 'individual' && dialog.users.includes(socket.email) && dialog.users.includes(user));
+
+    if (currentDialog) {
+      socket.emit('current-dialog', currentDialog);
+      socket.emit('messages', currentDialog.messages);
+      return;
+    }
+    const dialog = {
+      type: 'individual',
+      users: [socket.email, user],
+      messages: []
+    }
+    dialogs.push(dialog);
+    socket.emit('current-dialog', dialog);
+    socket.emit('messages', null);
+    const myDialogs = dialogs.filter(dialog => dialog.users.includes(socket.email));
+    socket.emit('dialogs', myDialogs);
+    const person = clients.find(client => client.email === user);
+    const personDialogs = dialogs.filter(dialog => dialog.users.includes(user));
+    person.emit('dialogs', personDialogs);
+
+  })
+  socket.on('change-dialog', dialog => {
+    const current = dialogs.find(i => i.type === dialog);
+    if (current) {
+      socket.emit('messages', current.messages);
+      socket.emit('current-dialog', current);
+      return;
+    }
+    const currentDialog = dialogs.find(i => i.type === 'individual' && i.users.includes(socket.email) && i.users.includes(dialog));
+    socket.emit('messages', currentDialog.messages);
+    socket.emit('current-dialog', currentDialog);
   })
 
   socket.on('block-user', (user) => {
@@ -124,6 +170,7 @@ io.on('connection', (socket) => {
     clients.forEach(client => {
       client.emit('people-online', emails); 
     })
+    dialogs[0].users = emails;
     const connect = rooms[socket.email];
     rooms[socket.email] = null;
     rooms[connect] = null;
